4382922c138a3d07a302f8e7952763f0e6f17f2f
Pulkit Gupta <gupta.pulkit89@gmail.com>
2012-10-02 15:05:47 -0400

    Update face_invitation

--- home/myuploads/face_invitation.php
+++ home/myuploads/face_invitation.php
@@ -5,6 +5,7 @@ if(isset($_REQUEST['u']))
 	$user_id = 	mysql_real_escape_string($_REQUEST['u']);
 	$email = 	mysql_real_escape_string($_REQUEST['v']);
 	$face_id = 	mysql_real_escape_string($_REQUEST['f']);
+	$result = mysql_query("UPDATE faces SET email = '$email' WHERE face_id = '$face_id'");
 	$result = mysql_query("SELECT * FROM users WHERE email = '$email'");
 	if($result == 0)
 		mysql_error($conn);
